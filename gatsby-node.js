4145c3013f321b526f548a9305d8b4f53bca3f60
Vince P <ianparulan@gmail.com>
2019-05-20 18:18:45 -0400

    added page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const loadPages = new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allContentfulPage {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+    `).then(result => {
+      const pages = result.data.allContentfulPage.edges
+
+      pages.map(({ node }) => {
+        createPage({
+          path: `${node.slug}/`,
+          component: path.resolve(`./src/templates/page-template.js`),
+          context: {
+            slug: node.slug,
+          }
+        })
+      })
+      resolve()
+    })
+  })
+
+  return Promise.all([loadPages])
+}
