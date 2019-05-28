41275bccf5496f27b3741fb5cd6124ff248e1484
Vince P <ianparulan@gmail.com>
2019-05-28 11:16:31 -0400

    added numbering and other shit

--- src/components/Sections/ProjectSection/index.js
+++ src/components/Sections/ProjectSection/index.js
@@ -1,38 +1,73 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
+import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 import { space, backgroundColor } from 'styled-system'
 
-import { mediaQueries, maxWidth, space as tokenSpace } from '../../../utils/tokens' 
+import { FaAngleRight } from 'react-icons/fa'
+
+import { fontSizes, mediaQueries, maxWidth, space as tokenSpace, colors, letterSpacings } from '../../../utils/tokens' 
 
 const ProjectCardRoot = styled.section`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
 `
 
 const CardTitle = styled.h2`
   ${space};
 `
 const ProjectCard = (props) => {
-  const { name, client, tags, media } = props
+  const { name, client, tags, media, number } = props
 
   return (
     <ProjectCardRoot>
-      <Img
-        fluid={media.fluid}
-        css={css`
-          flex: 1;
-          min-height: 450px;
-          max-height: 450px;
-        `}
-      />
+      <div css={css`
+        position: relative;
+        width: 100%;
+      `}>
+        {media ?
+          <Img
+            fluid={media.fluid}
+            alt={media.title}
+            css={css`
+            flex: 1;
+            min-height: 450px;
+            max-height: 450px;
+          `}
+          /> :
+          <div css={css`
+            min-height: 450px;
+            max-height: 450px;
+            background-color: ${colors.grey.reallyDark};
+          `}/>
+        }
+        <div css={css`
+          position: absolute;
+          bottom: 0;
+          right: 0;
+          height: ${tokenSpace[5]}px;
+          width: ${tokenSpace[5]}px;
+          background-color: ${colors.grey.light};
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}>
+          <span css={css`
+            font-weight: bold;
+            font-size: calc(${fontSizes[0]} - 2px);
+            letter-spacing: ${letterSpacings.mega};
+          `}>0{number}</span>
+        </div>
+      </div>
       <div>
         <CardTitle mt={4}>{name}</CardTitle>
         <div>
-          <span>Client: {client}</span>
+          {client && <span>Client: {client}</span>}
         </div>
         <div>
-          {tags.map((tag, i) => {
+          {tags && tags.map((tag, i) => {
             const isLast = tags.length === i + 1
             return (
               <span
@@ -42,14 +77,58 @@ const ProjectCard = (props) => {
           })}
         </div>
       </div>
+      <Link
+        to=""
+        css={css`
+          font-weight: bold;
+          font-size: calc(${fontSizes[0]} - 2px);
+          letter-spacing: ${letterSpacings.mega};
+          text-transform: uppercase;
+          display: flex;
+          align-items: center;
+          margin-top: ${tokenSpace[5]}px;
+        `}
+      >
+      <span css={css`
+        margin-right: ${tokenSpace[3]}px;
+      `}>Discover</span>
+      <FaAngleRight
+        css={css`
+          font-size: ${fontSizes[2]};
+          color: ${colors.accent};
+        `}
+      />
+      </Link>
     </ProjectCardRoot>
   )
+}
+
+ProjectCard.propTypes = {
+  name: PropTypes.string,
+  client: PropTypes.string,
+  tags: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+  })),
+  media: PropTypes.shape({
+    fluid: PropTypes.object,
+    title: PropTypes.string,
+  }),
+  number: PropTypes.number,
+}
+
+ProjectCard.defaultProps = {
+  name: 'Project name',
+  client: null,
+  tags: null,
+  media: null,
+  number: 0,
 }
 
 const Section = styled.section`
   ${space};
   ${backgroundColor};
 `
+
 const ProjectSection = (props) => {
   const { projectList } = props
   return (
@@ -79,7 +158,7 @@ const ProjectSection = (props) => {
               name={project.name}
               client={project.client}
               tags={project.tags}
-              media={project.coverPhoto}
+              number={index + 1}
             />
         )})}
       </div>
